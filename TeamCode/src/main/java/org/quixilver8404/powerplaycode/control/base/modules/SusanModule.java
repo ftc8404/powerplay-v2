7b50ad03f6551a1c1e7e901b577b958735353bba
Aidan Pak <aidanpak@gmail.com>
2022-12-09 15:59:30 -0800

    add setPosition

--- TeamCode/src/main/java/org/quixilver8404/powerplaycode/control/base/modules/SusanModule.java
+++ TeamCode/src/main/java/org/quixilver8404/powerplaycode/control/base/modules/SusanModule.java
@@ -7,6 +7,8 @@ public class SusanModule {
     public double power;
     public double position;
     public double desiredpos;
+    public double initialPos;
+    public double powerPos;
     public double leftbound;
     public double rightbound;
     public double tolerance;
@@ -31,6 +33,26 @@ public class SusanModule {
         if (position < leftbound && position > rightbound){
             hardwareMap.susanMotor1.setPower(scalar * power);
             hardwareMap.susanMotor2.setPower(scalar * power);
+        } else {
+            hardwareMap.susanMotor1.setPower(0);
+            hardwareMap.susanMotor2.setPower(0);
+        }
+    }
+
+    public synchronized void setInitialPosition(){
+        initialPos = position;
+    }
+
+    public synchronized void setPosition(double desiredPos){
+        if (position < leftbound && position > rightbound && Math.abs(position - desiredPos) > tolerance){
+            powerPos = Math.abs(desiredPos-position)/Math.abs(initialPos-desiredPos);
+            if (powerPos < 0.1) {
+                powerPos = 0.1;
+            } else if (powerPos > 0.8){
+                powerPos = 0.8;
+            }
+            hardwareMap.susanMotor1.setPower(powerPos);
+            hardwareMap.susanMotor2.setPower(powerPos);
         } else {
             hardwareMap.susanMotor1.setPower(0);
             hardwareMap.susanMotor2.setPower(0);
